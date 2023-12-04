50978cfd73d7c0cf370c65f246f8308531547bbb
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2023-12-04 10:17:22 +0600

    input form modified

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -4,19 +4,78 @@ const Signup = () => {
   return (
     <form action="#" className="sign-up-form">
       <h2 className="title">Sign up</h2>
-      <div className="input-field">
-        <i className="fas fa-user"></i>
-        <input type="text" placeholder="Username" />
+
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="email"
+          name="floating_email"
+          id="floating_email"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0
+             border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600
+              dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="floating_email"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400
+             duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0
+              rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600
+               peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Username
+        </label>
       </div>
-      <div className="input-field">
-        <i className="fas fa-envelope"></i>
-        <input type="email" placeholder="Email" />
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="email"
+          name="floating_email"
+          id="floating_email"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0
+             border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600
+              dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="floating_email"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400
+             duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0
+              rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600
+               peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Email address
+        </label>
       </div>
-      <div className="input-field">
-        <i className="fas fa-lock"></i>
-        <input type="password" placeholder="Password" />
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="password"
+          name="floating_password"
+          id="floating_password"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0
+             border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600
+              dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+        />
+        <label
+          htmlFor="floating_password"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400
+             duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0
+              rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500
+               peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Password
+        </label>
       </div>
-      <input type="submit" className="btn" value="Sign up" />
+      <button
+        type="button"
+        className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800
+         focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5
+          mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+      >
+        Sign Up
+      </button>
       <p className="social-text">Or Sign up with social platforms</p>
       <SocialMedia />
     </form>
