9d68c82086f5902f0408a718758b6f96aaf8e439
Jayed <137998593+jayeeed@users.noreply.github.com>
2023-12-05 17:45:02 +0600

    form

--- src/pages/Info/Info.jsx
+++ src/pages/Info/Info.jsx
@@ -1,74 +1,39 @@
 // Info.jsx
-import { useState } from "react";
-import FormInput from "./../../components/Form/FormInput";
-import img2 from "../../assets/register.svg";
+import React, { useState } from "react";
+import FormPersonal from "./../../components/Form/FormPersonal";
+import FormCompany from "./../../components/Form/FormCompany";
 
 const Info = () => {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    address: "",
-    country: "",
-    zip: "",
-    // Add more fields as needed
-  });
+  const [currentStep, setCurrentStep] = useState(1);
+  const [personalFormData, setPersonalFormData] = useState(null);
+  const [companyFormData, setCompanyFormData] = useState(null);
 
-  const [isSignUpMode, setSignUpMode] = useState(false);
-
-  const handleSignUpClick = () => {
-    setSignUpMode(true);
+  const handleNextPersonal = (data) => {
+    setPersonalFormData(data);
+    setCurrentStep((prevStep) => prevStep + 1);
   };
 
-  const handleSignInClick = () => {
-    setSignUpMode(false);
+  const handleNextCompany = (data) => {
+    setCompanyFormData(data);
+    setCurrentStep((prevStep) => prevStep + 1);
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add logic to submit the personal information data
-    console.log("Personal Information Submitted:", formData);
-    // You can make an API call to save the data to your server/database
+  const handlePrevious = () => {
+    setCurrentStep((prevStep) => prevStep - 1);
   };
 
   return (
-    <div className={`container ${isSignUpMode ? "sign-up-mode" : ""}`}>
-      <div className="forms-container">
-        <div className="personal-info-form signin-signup ml-12 mr-6">
-          <form onSubmit={handleSubmit}>
-            <h2 className="pb-8 text-2xl font-bold">Personal Information</h2>
-            <FormInput fieldName="First Name" />
-            <FormInput fieldName="LastName" />
-            <FormInput fieldName="Email" />
-            <FormInput fieldName="Phone" />
-            <FormInput fieldName="Address" />
-            <FormInput fieldName="Country" />
-            <FormInput fieldName="Zip" />
-            {/* Add more FormInput components as needed */}
-            <button
-              type="submit"
-              className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden
-             text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600
-              to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white"
-            >
-              <span
-                className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white
-                rounded-md group-hover:bg-opacity-0"
-              >
-                Submit
-              </span>
-            </button>
-          </form>
-        </div>
+    <div>
+      <h1 className="text-3xl font-semibold mb-4">Registration Page</h1>
+      <div>
+        {currentStep === 1 && <FormPersonal onNext={handleNextPersonal} />}
+        {currentStep === 2 && (
+          <FormCompany
+            onPrevious={handlePrevious}
+            onNext={handleNextCompany}
+            prevFormData={personalFormData}
+          />
+        )}
       </div>
     </div>
   );
