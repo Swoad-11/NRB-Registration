8a088bc85895c5e90dd9a49b78d46d88522049b2
jayeeed <jayedbinjahangir@gmail.com>
2023-12-06 15:43:06 +0600

    form route on social buttons

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -1,5 +1,6 @@
 import { FaFacebookF, FaGoogle } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaWpforms } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
 const SocialMedia = ({ value }) => {
@@ -30,6 +31,16 @@ const SocialMedia = ({ value }) => {
         >
           <FaGoogle />
           <span className="ml-2">Google</span>
+        </button>
+        {/* ###################### info form ###################### */}
+        <button
+          type="button"
+          className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2"
+        >
+          <Link to="/info" className="flex items-center">
+            <FaWpforms />
+            <span className="ml-2">Info Form</span>
+          </Link>
         </button>
       </div>
     </>
