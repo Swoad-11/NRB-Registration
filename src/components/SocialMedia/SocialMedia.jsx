25648c339eda9bb6f4bd74248e8fd05e2b9bd7be
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2023-12-13 12:51:27 +0600

    form button removed from social media component

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -1,10 +1,6 @@
 import { FaFacebookF, FaGoogle } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 
-// ############## test routes ##############
-import { FaWpforms } from "react-icons/fa";
-import { Link } from "react-router-dom";
-
 // eslint-disable-next-line react/prop-types
 const SocialMedia = ({ value }) => {
   return (
@@ -34,16 +30,6 @@ const SocialMedia = ({ value }) => {
         >
           <FaGoogle />
           <span className="ml-2">Google</span>
-        </button>
-        {/* ###################### info form ###################### */}
-        <button
-          type="button"
-          className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 mb-2"
-        >
-          <Link to="/info" className="flex items-center">
-            <FaWpforms />
-            <span className="ml-2">Form</span>
-          </Link>
         </button>
       </div>
     </>
